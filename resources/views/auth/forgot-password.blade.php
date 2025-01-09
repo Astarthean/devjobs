a21626f3ec4c7e11db8d16c2ab5219c6ba1bd3ee
Ana Bernal <asakalieu@gmail.com>
2025-01-09 10:21:50 +0100

    first commit

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,35 @@
+<x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __('¿Olvidaste tu contraseña? Escribe tu email de registro y te enviaremos un correo para que puedas crear una nueva') }}
+    </div>
+
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <div class="flex justify-between     my-4">
+            <x-link :href="route('login')">
+                ¿Ya estás registrado?
+            </x-link>
+            <x-link :href="route('register')">
+                Crear una cuenta
+            </x-link>
+        </div>
+
+        <div class="flex justify-end">
+            <x-primary-button>
+                {{ __('Recuperar contraseña') }}
+            </x-primary-button>
+        </div>
+
+    </form>
+</x-guest-layout>
