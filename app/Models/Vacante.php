7e18331011f7b73a77768d592876e91c89e1b2a7
Ana Bernal <asakalieu@gmail.com>
2025-01-21 12:18:49 +0100

    panel de administracion

--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vacante extends Model
 {
     use HasFactory;
+
+    protected $dates = ['ultimo_dia'];
 
     protected $fillable = [
         'titulo',
@@ -19,4 +22,9 @@ class Vacante extends Model
         'imagen',
         'user_id'
     ];
+
+    public function getUltimoDiaAttribute($value)
+    {
+        return Carbon::parse($value);  // Convierte el valor a Carbon
+    }
 }
---
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -0,0 +1,35 @@
+<div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 text-gray-900 dark:text-gray-100 md:flex md:justify-between md:items-center">
+                <div class="leading-10">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
+                    </a>
+                    <p class="text-m text-gray-500 font-bold">{{ $vacante->empresa }}</p>
+                    <p class="text-m text-gray-600 font-bold">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
+                </div>
+                <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+                    <a href="#"
+                        class="bg-indigo-700 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Candidatos
+                    </a>
+                    <a href="#"
+                        class="bg-gray-700 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Editar
+                    </a>
+                    <a href="#"
+                        class="bg-red-700 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Eliminar
+                    </a>
+                </div>
+            </div>
+        @empty
+            <p class="p-3 text-center text-m text-gray-600">No hay Vacantes disponibles que mostrar</p>
+        @endforelse
+    </div>
+
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
+</div>
